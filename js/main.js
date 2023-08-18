2091fd7f386c6b9a55af486842c9251f484d0a70
ulugbekdev1107 <ulugbekdev1107@gmail.com>
2023-08-18 11:23:18 +0500

    tayyor barcha file

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const btnClass = document.getElementsByClassName('btn')
+const modals = document.querySelector('.modals')
+const active = document.getElementsByClassName('active')
+
+
+const btnModals = () => {
+ modals.classList.toggle('active');
+}
